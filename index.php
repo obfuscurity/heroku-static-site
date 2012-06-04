2c01d254d9989837ef575dccde470b2cf8521b8d
obfuscurity <jdixon@heroku.com>
2012-06-03 20:20:44 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Static Site on Heroku</title>
+  </head>
+  <body>
+    <h1>Static Site on Heroku</h1>
+    <ol>
+      <li>Create your <tt>index.php</tt>, just like this one.</li>
+      <li>Create your heroku app.</li>
+      <li>Commit and push your <tt>index.php</tt> file to your heroku app.</li>
+    </ol>
+  </body>
+</html>
